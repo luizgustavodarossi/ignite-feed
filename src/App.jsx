7697d760984cc999d21ea8cdc983b5d2c9c672fc
Luiz Gustavo Retka Darossi <luizgustavodarossi@gmail.com>
2022-06-19 11:20:56 -0300

    estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Header } from "./components/Header"
+
+import './global.css'
+
 function App() {
 
   return (
-    <h1>Hello World</h1>
+    <Header />
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import {header}  from './Header.module.css'
+
+export function Header(){
+    return (
+        <header>
+            <h1 className={header}>Header</h1>
+        </header>
+    )
+}
