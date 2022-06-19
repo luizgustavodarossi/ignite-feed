bf2a69b5bbef3d4a975c7da83fa00d9167ebfe1c
Luiz Gustavo Retka Darossi <luizgustavodarossi@gmail.com>
2022-06-19 14:32:47 -0300

    component sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { Header } from "./components/Header"
+import { Post } from "./components/Post"
+import { Sidebar } from './components/Sidebar'
 
 import './global.css'
+import styles from './App.module.css'
 
-function App() {
+export function App() {
 
   return (
-    <Header />
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post 
+            author="Luiz Gustavo"
+            content="Um post bacana" 
+          />
+          <Post 
+            author="Diego Fernandes"
+            content="Um post dahora" 
+          />
+        </main>
+      </div>
+    </>
   )
 }
-
-export default App
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import {header}  from './Header.module.css'
-
-export function Header(){
-    return (
-        <header>
-            <h1 className={header}>Header</h1>
-        </header>
-    )
-}
